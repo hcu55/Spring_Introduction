f11a4bc96727f00f25c43ac1b5168008b02025f1
ChanUi <hcu55@naver.com>
2023-09-10 19:50:23 +0900

    :sparkles: MemoryMemberRepository Testcase 작성

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -32,4 +32,8 @@ public class MemoryMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());     // store에 있는 값을 반환하면 된다. = Member
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
