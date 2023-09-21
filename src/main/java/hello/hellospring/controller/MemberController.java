eeb8b8ec387f3b97e962f3a8ab41bc7245b8cdbb
ChanUi <hcu55@naver.com>
2023-09-22 03:50:15 +0900

    :sparkles: AOP 적용 구현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -18,6 +18,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+        System.out.println("memberService = " + memberService.getClass());      //실제 Proxy가 주입되는지 콘솔에 출력해서 확인하기
     }
 
     @GetMapping("/members/new")
